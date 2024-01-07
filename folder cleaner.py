f2916ae7f2d3b135df48de3a0fff0b08623cb0e6
kilstryke <152299837+kilstryke@users.noreply.github.com>
2024-01-07 14:24:05 -0400

    Update folder cleaner.py

--- folder cleaner.py
+++ folder cleaner.py
@@ -2,13 +2,13 @@ import os
 import shutil
 list = []
 i = 0 
-destinationdir = 'F:\\Araf\\Cleanup'
+destinationdir = 'folder name'
 while os.path.exists(destinationdir):
     destinationdir += str(i)
     i+=1
 os.makedirs(destinationdir)
 os.path.abspath(__file__)
-list = os.listdir('F:\\Araf\\Cleanup')
+list = os.listdir('folder name')
 for x in list:
     print (x)
     if x == __file__:
